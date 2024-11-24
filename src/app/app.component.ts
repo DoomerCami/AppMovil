8389473d34524c0d13ad1e6576c079bccf131024
DoomerCami <camily.muso.diaz@utelvt.edu.ec>
2024-11-24 03:10:34 -0500

    cambiso x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent,IonMenuToggle, IonMenu, IonButton} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonContent, IonTitle, IonToolbar, IonHeader, IonApp, IonRouterOutlet,IonMenuToggle, IonMenu, IonButton],
 })
 export class AppComponent {
   constructor() {}
--- src/app/product-list/product-list.page.ts
+++ src/app/product-list/product-list.page.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { RouterLink } from '@angular/router';
+
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.page.html',
-  styleUrls: ['./main.page.scss'],
+  selector: 'app-product-list',
+  templateUrl: './product-list.page.html',
+  styleUrls: ['./product-list.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class MainPage implements OnInit {
+export class ProductListPage implements OnInit {
 
   constructor() { }
 
